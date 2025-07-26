7756ace9e3e4d7606d87c3acf3688a0260e1e02d
YusufjonovAyyomjon <yusufjonovayyomjon@gmail.com>
2025-07-26 11:03:50 +0500

    homwork

---
+++ homwork2/index.js
@@ -0,0 +1,19 @@
+// 1
+//let stringToArray = (string) => string.split(' ');
+
+
+//2
+//let invert=(array) =>  array.map(n => -n);
+
+
+//3
+//const abbrevName = name => name[0].toUpperCase() + '.' + name[name.indexOf(' ') + 1].toUpperCase();
+
+
+//4
+// let makeUpperCase=(str)=> str.toUpperCase();
+
+
+
+//5
+//let greet = (name)=> `Hello, ${name} how are you doing today?`
